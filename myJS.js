ae00bb1f79e417be47b2bff0f4fb08c94fd353b5
crea <masataka0504@gmail.com>
2017-08-06 19:59:17 -0700

    2nd commit

--- myJS.js
+++ myJS.js
@@ -1,62 +1,3 @@
-// error でてるが　うごく　ここでjquery インポートとか　変なことしなくてもよい
-
-
-// １。　これ　きいてる
-// var shiftWindow = function(){
-//     // alert('kiteru');
-//    scrollBy(0, -50);
-//  };
-//
-// window.addEventListener("hashchange", shiftWindow);
-//
-// function load(){
-//   alert('kiterudd');
-//   if(window.location.hash)
-//    shiftWindow();
-// }
-
-
-// 2. これも　きいてる
-
-
-// $(window).load(function () {
-//   if ($(".navbar.navbar-fixed-top").length > 0) {
-//     // var navHeight = $(".navbar").height(),
-//     var navHeight = 50,
-//       shiftWindow = function() { scrollBy(0, -navHeight - 10); };
-//     if (location.hash) {
-//       setTimeout(shiftWindow, 1);
-//     }
-//     window.addEventListener("hashchange", shiftWindow);
-//   }
-// });
-
-
-
-
-
-
-//スムーズスクロール
-// jQuery(function(){
-// 	// ★　任意のズレ高さピクセル数を入力　↓
-// 	var headerHight = 100;
-//    // #で始まるアンカーをクリックした場合に処理
-//    jQuery('a[href^=#]').click(function() {
-// 	  // スクロールの速度
-// 	  var speed = 1000; // ミリ秒
-// 	  // アンカーの値取得
-// 	  var href= jQuery(this).attr("href");
-// 	  // 移動先を取得
-// 	  var target = jQuery(href === "#" || href === "" ? 'html' : href);
-// 	  // 移動先を数値で取得
-// 	  var position = target.offset().top-headerHight; // ※　-headerHightでズレの処理
-// 	  // スムーズスクロール
-// 	  jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
-// 	  return false;
-//    });
-// });
-
-
 
 $(function(){
 
@@ -65,8 +6,7 @@ $(function(){
   //  	var headerHight = 70;
   // }
 
-
-   var headerHight = 0;
+  var headerHight = 0;
 
    // #で始まるアンカーをクリックした場合に処理
    $('a[href^="#"]').click(function() {
@@ -118,9 +58,6 @@ $(function(){
 
       // console.log(headerHight);
 
-
-
-
       // 移動先を取得
       var target = $(href === "#" || href === "" ? 'html' : href);
       // 移動先を数値で取得
@@ -130,8 +67,6 @@ $(function(){
         //  alert("tasukete");
       return false;
    });
-
-
 
 
    $(document).ready(function() {
@@ -158,25 +93,4 @@ $(function(){
        transition: function(url){ window.location.href = url; }
      });
    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
